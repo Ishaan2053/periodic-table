d8a430268e770ae5bf44e27e630eafba925ea806
Ishaan2053 <Ishaan2053@gmail.com>
2025-05-18 19:57:15 +0530

    feat: add MagneticEffect and Dialog components with styling, and update package dependencies

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,9 +22,10 @@ const geistMono = Geist_Mono({
 export default function Home() {
   // Initialize Lenis smooth scrolling
   const lenis = useLenisScroll();
-  
+
   const [selectedElement, setSelectedElement] = useState<Element | null>(null);
-  const [selectedCategory, setSelectedCategory] = useState<ElementCategory | null>(null);
+  const [selectedCategory, setSelectedCategory] =
+    useState<ElementCategory | null>(null);
 
   const categoryColors: Record<ElementCategory, string> = {
     nonmetal: "bg-lime-400",
@@ -61,54 +62,66 @@ export default function Home() {
   };
 
   return (
-    <main className={`bg-gradient-to-b from-slate-950 to-neutral-950 mx-auto flex min-h-screen flex-col items-center justify-between p-2 globalScroll`}>
+    <main
+      className={`bg-gradient-to-b from-slate-950 to-neutral-950 mx-auto flex min-h-screen flex-col items-center justify-between p-2 `}
+    >
       <Head>
         <title>Interactive Periodic Table</title>
         <meta name="description" content="Interactive Periodic Table" />
         <link rel="icon" href="/favicon.ico" />
-
       </Head>
       {/* <h1 className="text-red-500 text-4xl">Interactive Periodic Table</h1>
       <h3>Hover over elements to see their atomic structure and know more</h3> */}
 
-      {/* LEGEND */}
-      <div className="flex flex-wrap gap-3 w-full p-4 rounded-lg my-4 justify-center">
-        {Object.entries(categoryColors).map(([category, colorClass]) => (
-          <div 
-            key={category} 
-            className={`flex items-center gap-2 cursor-pointer px-2 py-1 rounded ${selectedCategory === category ? 'bg-gray-800' : ''}`}
-            onClick={() => handleLegendClick(category as ElementCategory)}
-          >
-            <div className={`${colorClass} w-4 h-4 rounded-sm`}></div>
-            <span className="text-white capitalize text-sm">
-              {category.replace(/-/g, " ")}
-            </span>
-          </div>
-        ))}
+      <div className="flex md:hidden items-center text-center justify-center text-semibold text-white flex-col text-2xl"> 
+        Switch to desktop mode to view the periodic table
+
       </div>
 
-      <div className="{styles.tableContainer}">
-        <PeriodicTable 
-          onElementSelect={setSelectedElement} 
-          selectedCategory={selectedCategory} 
+      <div className="hidden md:flex flex-col items-center justify-center">
+        {/* LEGEND */}
+        <div className="flex flex-wrap gap-3 w-full p-4 rounded-lg my-4 justify-center">
+          {Object.entries(categoryColors).map(([category, colorClass]) => (
+            <div
+              key={category}
+              className={`flex items-center gap-2 cursor-pointer px-2 py-1 rounded ${
+                selectedCategory === category ? "bg-gray-800" : ""
+              }`}
+              onClick={() => handleLegendClick(category as ElementCategory)}
+            >
+              <div className={`${colorClass} w-4 h-4 rounded-sm`}></div>
+              <span className="text-white capitalize text-sm">
+                {category.replace(/-/g, " ")}
+              </span>
+            </div>
+          ))}
+        </div>
+
+        <div className="{styles.tableContainer}">
+          <PeriodicTable
+            onElementSelect={setSelectedElement}
+            selectedCategory={selectedCategory}
+          />
+        </div>
+
+        {selectedElement && (
+          <div className=''>
+            <ElementDetail
+              element={selectedElement}
+              showAtomicStructure={true}
+            />
+          </div>
+        )}
+      </div>
+      <div className="flex items-center justify-center">
+        <FloatingDock
+          items={links}
         />
       </div>
-
-      {selectedElement && (
-        <div className="{styles.detailsContainer}">
-          <ElementDetail element={selectedElement} showAtomicStructure={true} />
-        </div>
-      )}
-       <div className="flex items-center justify-center">
-      <FloatingDock
-        mobileClassName="translate-y-20" // only for demo, remove for production
-        items={links}
-      />
-    </div>
       <div className="w-full flex justify-center items-center">
         <p className="text-gray-400 text-sm">
           Made by{" "}
-          <Link href="github.com/ishaan2053">
+          <Link href="https://www.ishaan2053.tech/" target="_blank">
             Ishaan2053
           </Link>
         </p>
